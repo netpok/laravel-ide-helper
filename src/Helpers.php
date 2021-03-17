e5e2ed4ede2ace02148bae00f2fc7ed8cefc2d10
Balázs Váradi <netpok@gmail.com>
2021-03-17 08:40:50 +0100

    Add deprecation notice

--- src/Helpers.php
+++ src/Helpers.php
@@ -6,6 +6,10 @@ namespace Barryvdh\LaravelIdeHelper;
 
 class Helpers
 {
+    /**
+     * @deprecated This method is no longer in use and will be removed in upcoming versions.
+     * @return bool
+     */
     public static function isLaravel(): bool
     {
         return class_exists('Illuminate\Foundation\Application');
